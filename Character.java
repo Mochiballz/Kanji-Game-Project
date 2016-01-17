9830f97f8c97e2d3e2426070473db28c353bb194
Mochiballz <aaronjustinsanders@gmail.com>
2016-01-17 13:22:27 -0600

    Addedprivate variables and constructor to Character.java

--- Character.java
+++ Character.java
@@ -1,4 +1,30 @@
+
+// Character class:
+// - would take in index number (Unicode #, JIS #, etc.)
+// - character itself
+// - onyomi and kunyomi
+// - English meaning
+// - example words? + furigana reading
+
+import java.util.*;
+
 public class Character {
+
+  private int indexNum;
+  private String kanjiChar;
+  private String onyomi;
+  private String kunyomi;
+  private String englishMean;
+  private ArrayList<String> examples;
+
+  public Character() {
+    indexNum = 0;
+    kanjiChar = "";
+    onyomi = "";
+    kunyomi = "";
+    englishMean = "";
+    examples = new ArrayList();
+  }
 
   public static void main (String [] args) {
 
