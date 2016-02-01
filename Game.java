9dfbaceb560eab0057e002cf6dcdbdaacc3ad0d9
Mochiballz <aaronjustinsanders@gmail.com>
2016-01-31 20:45:52 -0600

    Added readFileToArrayList function to Game.java

--- Game.java
+++ Game.java
@@ -5,8 +5,25 @@ import java.io.*;
 
 public class Game {
 
+  // Takes file, turns lines to elements of ArrayList<String>
+  public static ArrayList<String> readFileToArrayList(String file) {
+    ArrayList<String> list = new ArrayList<String>();
+    try {
+      Scanner s = new Scanner(new File(file));
+      while(s.hasNextLine()) {
+        list.add(s.nextLine());
+      }
+      s.close();
+    }
+    catch(Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return list;
+  }
+
   public static void main(String[] args) {
-    
+    // Prints ArrayList<String> of INDEX.txt
+    // System.out.println(readFileToArrayList("INDEX.txt"));
   }
 
 }
